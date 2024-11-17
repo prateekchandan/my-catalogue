445d196597093fd5c8ee3c5b395ef80c2c1e2ca9
Prateek Chandan <prateekchandan5545@gmail.com>
2024-11-17 13:07:17 +0530

    Fixed bug

--- script/script.js
+++ script/script.js
@@ -3,43 +3,46 @@ fetch('data/data.json')
   .then(products => {
     const featuredProducts = products.filter(product => product.featured);
     const featuredProductsContainer = document.getElementById('featured-products');   
-
-
-    featuredProducts.forEach(product => {
-      const productHTML = `
-        <div class="col-md-4">
-          <div class="card">
-            <img src="${product.image}" class="card-img-top" alt="${product.name}">
-            <div class="card-body">
-              <h5 class="card-title">${product.name}</h5>
-              <p class="card-text">${product.description}</p>   
-
-              <a href="#" class="btn btn-primary">View Details</a>
-            </div>
-          </div>
-        </div>   
-
-      `;
-      featuredProductsContainer.innerHTML += productHTML;
-    });
-
     const allProductsContainer = document.getElementById('all-products');
 
-    products.forEach(product => {
-      const productHTML = `
-        <div class="col-md-4">
-          <div class="card">
-            <img src="${product.image}" class="card-img-top" alt="${product.name}">
-            <div class="card-body">
-              <h5 class="card-title">${product.name}</h5>
-              <p class="card-text">${product.description}</p>   
 
-              <a href="#" class="btn btn-primary">View Details</a>
+    if (featuredProductsContainer) {
+      featuredProducts.forEach(product => {
+        const productHTML = `
+          <div class="col-md-4">
+            <div class="card">
+              <img src="${product.image}" class="card-img-top" alt="${product.name}">
+              <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.description}</p>   
+  
+                <a href="#" class="btn btn-primary">View Details</a>
+              </div>
             </div>
-          </div>
-        </div>   
+          </div>   
+  
+        `;
+        featuredProductsContainer.innerHTML += productHTML;
+      });
+    }
+    
+    if (allProductsContainer) {
+      products.forEach(product => {
+        const productHTML = `
+          <div class="col-md-4">
+            <div class="card">
+              <img src="${product.image}" class="card-img-top" alt="${product.name}">
+              <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.description}</p>   
 
-      `;
-      allProductsContainer.innerHTML += productHTML;
-    });
+                <a href="#" class="btn btn-primary">View Details</a>
+              </div>
+            </div>
+          </div>   
+
+        `;
+        allProductsContainer.innerHTML += productHTML;
+      });
+    }
   });
